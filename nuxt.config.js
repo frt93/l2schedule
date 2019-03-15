3422feeb65935a384c5f6f445cb823ca11e9b9d2
frt <ectoprime@gmail.com>
2019-03-15 18:06:03 +0200

    v 1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ module.exports = {
    **Axios module config
    */
   axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:3333/api/',
+    baseURL: process.env.BASE_URL || '/api/',
     retry: { retries: 5 },
     debug: false,
   },
